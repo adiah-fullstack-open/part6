68b46c3f79a4f88d5feb9cd2cf4753f49a2d4aa1
nathanieladiah <nathanieladiah@gmail.com>
2024-07-06 06:57:50 -0400

    Retrieve anecdotes from backend

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,11 +1,22 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdotes";
 
 const App = () => {
   const notification = useSelector((state) => state.notification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>
